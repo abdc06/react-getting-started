b5ded8b041c32c731ffb40c6f05b22c417b88c2f
abdc06 <abdc2806@gmail.com>
2022-11-09 12:49:22 +0900

    useState Counting

--- react-simple/src/App.js
+++ react-simple/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Counter from './pages/Counter';
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
       </Routes>
     </div>
   );
