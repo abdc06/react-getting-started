f390a13528656f3d2f0c560ba4c3160f019b10a2
abdc06 <abdc2806@gmail.com>
2022-11-21 22:27:55 +0900

    create sudoku matrix

--- sudoku-app/src/App.js
+++ sudoku-app/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [matrix, setMatrix] = useState(
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+  );
+
+  function onCellChange(rowIndex, colIndex, value) {
+    let newMatrix = [...matrix];
+    newMatrix[rowIndex][colIndex] = value;
+    setMatrix(newMatrix);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h3>Soduku-App</h3>
+        <table>
+          <tbody>
+            {
+              matrix.map((row, rowIndex) => {
+                return <tr key={rowIndex}>
+                  {
+                    row.map((col, colIndex) => {
+                      return <td key={rowIndex + colIndex}>
+                        <input className='cellInput' value={col} onChange={(e) => onCellChange(rowIndex, colIndex, parseInt(e.target.value) || 0)}/>
+                      </td>
+                    })
+                  }
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
