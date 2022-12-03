783507168b354543ca3548c1aa43471bdd96e882
abdc06 <abdc2806@gmail.com>
2022-12-03 23:03:25 +0900

    apply comment styles

--- first-react/src/section05/Comment.jsx
+++ first-react/src/section05/Comment.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid grey",
+        borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    contentContainer: {
+        marginLeft: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    nameText: {
+        color: "black",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    commentText: {
+        color: "black",
+        fontSize: 16,
+    }
+};
+
 const Comment = (props) => {
     return (
-        <div>
-            <h1>{props.content}</h1>
-            <span>{props.name}</span>
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                    style={styles.image}
+                />
+            </div>
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>{props.content}</span>
+                <span style={styles.commentText}>{props.name}</span>
+            </div>
         </div>
     )
 }
